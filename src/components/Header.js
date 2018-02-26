a160c9d1a653904dbc43ec95bdc76ae300058c5d
sssmi <simeon.kerkola@gmail.com>
2018-02-26 21:18:31 +0200

    Style the Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,18 @@ export const Header = () => (
     <div className="content-container">
       <div className="header__content">
         {/* <NavLink to="/" activeClassName="is-active" exact>Home</NavLink> */}
-        <Link className="header__title" to="/dashboard">
-          <h1>Boilerplate</h1>
-        </Link>
+        <div className="header__logo-box">
+          <img src="/images/logo-white.png" alt="Logo" className="header__logo" />
+        </div>
+        <div className="content-container--center">
+          <h1 className="heading-primary">
+            <span className="heading-primary__main">Outdoors</span>
+            <span className="heading-primary__sub"> is where life happens</span>
+          </h1>
+          <Link className="header__title" to="/">
+            <button>button</button>
+          </Link>
+        </div>
       </div>
     </div>
   </header>
