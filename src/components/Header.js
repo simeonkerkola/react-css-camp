c090f3d9fcb356516cd77807fd82006eac58a10b
sssmi <simeon.kerkola@gmail.com>
2018-03-01 20:09:32 +0200

    Fix BEM

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,22 +3,18 @@ import { NavLink, Link } from 'react-router-dom';
 
 export const Header = () => (
   <header className="header">
-    <div className="content-container">
-      <div className="header__content">
-        {/* <NavLink to="/" activeClassName="is-active" exact>Home</NavLink> */}
-        <div className="header__logo-box">
-          <img src="/images/logo-white.png" alt="Logo" className="header__logo" />
-        </div>
-        <div className="content-container--center">
-          <h1 className="heading-primary">
-            <span className="heading-primary__main">Outdoors</span>
-            <span className="heading-primary__sub">is where life happens</span>
-          </h1>
-          <Link className="header__title" to="/">
-            <button className="btn btn--white btn--animated">Discover our tours</button>
-          </Link>
-        </div>
-      </div>
+    {/* <NavLink to="/" activeClassName="is-active" exact>Home</NavLink> */}
+    <div className="header__logo-box">
+      <img src="/images/logo-white.png" alt="Logo" className="header__logo" />
+    </div>
+    <div className="header__text-box--center">
+      <h1 className="heading-primary">
+        <span className="heading-primary--main">Outdoors</span>
+        <span className="heading-primary--sub">is where life happens</span>
+      </h1>
+      <Link to="/">
+        <button className="btn btn--white btn--animated">Discover our tours</button>
+      </Link>
     </div>
   </header>
 );
