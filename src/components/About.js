93bff8e52ad5404926cae300e30cfceb6dac685a
sssmi <simeon.kerkola@gmail.com>
2018-03-04 20:10:59 +0200

    Edit some firebase stuff for heroku

--- src/components/About.js
+++ src/components/About.js
@@ -6,8 +6,21 @@ const About = () => (
       <h2 className="heading-secondary">Exciting tours for adventurous people</h2>
     </div>
     <div className="row">
-      <div className="col-1-of-2">col 1-of-2</div>
-      <div className="col-1-of-2">col 1-of-2</div>
+      <div className="col-1-of-2">
+        <h3 className="heading-tertiary">You are going to fall in love with nature.</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste quidem quas incidunt est
+          officiis sed doloremque neque eligendi, rem architecto laborum deleniti molestias fugit
+          tempora asperiores tempore, quae doloribus! At.
+        </p>
+        <h3 className="heading-tertiary">Live adventures like you never had before.</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio, earum dolorum voluptatibus
+          adipisci eum atque totam iusto explicabo consequuntur tempora debitis sunt, maxime
+          accusamus quo pariatur omnis ea tenetur. Deserunt.
+        </p>
+      </div>
+      <div className="col-1-of-2" />
     </div>
   </section>
 );
