40894fe71d8f7434cdfe85a3421fb0f849f964b8
sssmi <simeon.kerkola@gmail.com>
2018-03-07 23:02:24 +0200

    Rename cards selector

--- src/components/Tours.js
+++ src/components/Tours.js
@@ -6,21 +6,25 @@ const Tours = () => (
       <h2 className="heading-secondary">Most popular tours</h2>
       <div className="row">
         <div className="col-1-of-3">
-          <div className="tour-card">
-            <div className="tour-card__side tour-card__side--front">front</div>
-            <div className="tour-card__side tour-card__side--back">back</div>
+          <div className="card">
+            <div className="card__side card__side--front">
+              <div className="card__picture" />
+              <div className="card__heading">Heading</div>
+              <div className="card__details">Details</div>
+            </div>
+            <div className="card__side card__side--back">back</div>
           </div>
         </div>
         <div className="col-1-of-3">
-          {/* <div className="tour-card">
-            <div className="tour-card__side tour-card__side--front">front</div>
-            <div className="tour-card__side tour-card__side--back">back</div>
+          {/* <div className="card">
+            <div className="card__side card__side--front">front</div>
+            <div className="card__side card__side--back">back</div>
           </div> */}
         </div>
         <div className="col-1-of-3">
-          {/* <div className="tour-card">
-            <div className="tour-card__side tour-card__side--front">front</div>
-            <div className="tour-card__side tour-card__side--back">back</div>
+          {/* <div className="card">
+            <div className="card__side card__side--front">front</div>
+            <div className="card__side card__side--back">back</div>
           </div> */}
         </div>
       </div>
