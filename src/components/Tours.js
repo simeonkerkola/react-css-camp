536f39f4b2cf93b427fe2aa8b527dcb40d1474e0
sssmi <simeon.kerkola@gmail.com>
2018-03-08 10:33:12 +0200

    Add Css Autoprefixed

--- src/components/Tours.js
+++ src/components/Tours.js
@@ -8,7 +8,9 @@ const Tours = () => (
         <div className="col-1-of-3">
           <div className="card">
             <div className="card__side card__side--front">
-              <div className="card__picture" />
+              <div className="card__picture card__picture--1">
+                <br />
+              </div>
               <div className="card__heading">Heading</div>
               <div className="card__details">Details</div>
             </div>
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,20 @@
-const path = require('path')
-const webpack = require('webpack')
-const DotEnv = require('dotenv')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const path = require('path');
+const webpack = require('webpack');
+const DotEnv = require('dotenv');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const autoprefixer = require('autoprefixer');
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 if (process.env.NODE_ENV === 'test') {
-  DotEnv.config({ path: '.env.test' })
+  DotEnv.config({ path: '.env.test' });
 } else if (process.env.NODE_ENV === 'development') {
-  DotEnv.config({ path: '.env.development' })
+  DotEnv.config({ path: '.env.development' });
 }
 
 module.exports = (env) => {
-  const isProduction = env === 'production'
-  const CSSExtract = new ExtractTextPlugin('styles.css')
+  const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css');
 
   return {
     entry: ['babel-polyfill', './src/app.js'], // babel-polyfill first to add support for older browsers
@@ -33,17 +34,29 @@ module.exports = (env) => {
           // use allows us to use an array of loaders
           use: CSSExtract.extract({
             // loaders go inside a plugin
-            use: [{
-              loader: 'css-loader',
-              options: {
-                sourceMap: true, // enable sourcemap support for more dev friendly environment
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  sourceMap: true, // enable sourcemap support for more dev friendly environment
+                },
               },
-            }, {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: true,
+              {
+                loader: 'postcss-loader',
+                options: {
+                  plugins() {
+                    return [autoprefixer];
+                  },
+                  sourceMap: true,
+                },
               },
-            }],
+              {
+                loader: 'sass-loader',
+                options: {
+                  sourceMap: true,
+                },
+              },
+            ],
           }),
         },
       ],
@@ -57,7 +70,9 @@ module.exports = (env) => {
         'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
         'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
         'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(
+          process.env.FIREBASE_MESSAGING_SENDER_ID,
+        ),
       }),
     ],
     devtool: isProduction ? 'source-map' : 'inline-source-map', // check if production, implement sourcemap
@@ -66,5 +81,5 @@ module.exports = (env) => {
       historyApiFallback: true, // tells dev-server we r handling routing via client side code (serve index.html every time we get a 404)
       publicPath: '/dist/',
     },
-  }
-}
+  };
+};
