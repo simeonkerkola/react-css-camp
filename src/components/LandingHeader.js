62fea20e30143cb9669fd76a4be086d72cd93d9f
sssmi <simeon.kerkola@gmail.com>
2018-03-09 23:58:34 +0200

    Edit utilities

--- src/components/LandingHeader.js
+++ src/components/LandingHeader.js
@@ -7,7 +7,7 @@ export const Header = () => (
     <div className="header__logo-box">
       <img src="/images/logo-white.png" alt="Logo" className="header__logo" />
     </div>
-    <div className="header__text-box--center">
+    <div className="text-box--center">
       <h1 className="heading-primary">
         <span className="heading-primary--main">Outdoors</span>
         <span className="heading-primary--sub">is where life happens</span>
