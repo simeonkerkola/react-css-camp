49fb3c445f2f0fe62a497206482b17b7a6dc9120
sssmi <simeon.kerkola@gmail.com>
2018-03-28 21:34:06 +0300

    Heading font for smaller screens

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,19 @@ import React from 'react';
 const Footer = () => (
   <footer className="footer">
     <div className="footer__logo-box">
-      <img src="/images/logo-green-2x.png" className="footer__logo" alt="logo" />
+      <picture className="footer__logo">
+        {/* In source element, we can write a Media query for phone */}
+        <source
+          srcSet="/images/logo-green-small-1x.png 1x, /images/logo-green-small-2x.png 2x"
+          media="(max-width: 37.5em)"
+        />
+        {/* Let the browser to decide which image to use according to screen resolution */}
+        <img
+          srcSet="/images/logo-green-1x.png 1x, /images/logo-green-2x.png 2x"
+          className="footer__logo"
+          alt="logo"
+        />
+      </picture>
     </div>
     <div className="row">
       <div className="col-1-of-2">
